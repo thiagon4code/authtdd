a39d5b0d1440f342e41e12e0b630c6c656692ffe
Thiago Roberto <thiago.roberto@perfectflightapp.com>
2021-03-11 20:01:33 -0300

    adding tests

---
+++ __tests__/integration/session.test.js
@@ -0,0 +1,11 @@
+describe("Authentication", () => {
+  // it("should receive JWT token when authenticated with valid credentials", () => {});
+  it("should sum two numbers", () => {
+    const x = 2;
+    const y = 4;
+
+    const sum = x + y;
+
+    expect(sum).toBe(7);
+  });
+});
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,3 @@
 const routes = require("express").Router();
 
-// definition routes
-// https://www.youtube.com/watch?v=2G_mWfG0DZE
-
 module.exports = routes;
